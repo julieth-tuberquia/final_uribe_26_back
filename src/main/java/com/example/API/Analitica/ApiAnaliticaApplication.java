bc4d38c466aab5aa169dd82d00a756d824b46472
julieth tuberquia <zarachoe123@gmail.com>
2026-03-04 13:47:00 -0500

    feat creacion de la base del proyecto de spring boot

---
+++ src/main/java/com/example/API/Analitica/ApiAnaliticaApplication.java
@@ -0,0 +1,13 @@
+package com.example.API.Analitica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAnaliticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAnaliticaApplication.class, args);
+	}
+
+}
